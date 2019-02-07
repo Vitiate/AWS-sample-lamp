886afda574e348c3c3a45aee6dec9ce4acbbaf7a
Jeremy Tirrell <jeremy@tirrell.ca>
2019-02-07 10:44:26 -0700

    DOH!

--- make_scripts/update_service.sh
+++ make_scripts/update_service.sh
@@ -1,3 +1,3 @@
-!#/bin/bash
+#!/bin/bash
 ECSName=$(aws ssm get-parameter --name "/cloudformation/applications/$ENV/ecs-cluster/cluster/name" --output text --query Parameter.Value)
 aws ecs update-service --cluster $ECSName --service "$1" --force-new-deployment
